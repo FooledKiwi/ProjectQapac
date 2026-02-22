d949c7dc079f60e0b692f359addddae2954903f8
Galaxy <luisirigoin123@gmail.com>
2026-02-22 12:16:25 -0500

    First Time activity now only appears on first time

--- mobile/ProjectQapacApp/app/src/main/java/com/fooledkiwi/projectqapacapp/auth/FirstTimeActivity.java
+++ mobile/ProjectQapacApp/app/src/main/java/com/fooledkiwi/projectqapacapp/auth/FirstTimeActivity.java
@@ -1,6 +1,7 @@
 package com.fooledkiwi.projectqapacapp.auth;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,10 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.fooledkiwi.projectqapacapp.MainActivity;
 import com.fooledkiwi.projectqapacapp.R;
 
 public class FirstTimeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +25,12 @@ public class FirstTimeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if(!checkIfFirstTime()) {
+            gotoMain();
+            /// Acá va a iniciar sesión automáticamente.
+            finish();
+        }
     }
 
     public void registerBtn(View vw) {
@@ -42,5 +49,24 @@ public class FirstTimeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AuthActivity.class);
         intent.putExtra("TAB_POS", action);
         startActivity(intent);
+    }
+
+    public void gotoMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public boolean checkIfFirstTime() {
+        SharedPreferences prefs = getSharedPreferences("QapacPrefs", MODE_PRIVATE);
+        boolean first = prefs.getBoolean("first_time", true);
+        if (first) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("first_time", false);
+            editor.apply();
+            return true;
+        } else {
+
+            return false;
+        }
     }
 }
