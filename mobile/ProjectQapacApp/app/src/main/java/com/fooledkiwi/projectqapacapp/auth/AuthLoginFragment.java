7e78bbcac71595ca3298c720e0eb484368a5f0c9
Galaxy <luisirigoin123@gmail.com>
2026-02-22 11:30:00 -0500

    Minor changes

--- mobile/ProjectQapacApp/app/src/main/java/com/fooledkiwi/projectqapacapp/auth/AuthLoginFragment.java
+++ mobile/ProjectQapacApp/app/src/main/java/com/fooledkiwi/projectqapacapp/auth/AuthLoginFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.fooledkiwi.projectqapacapp.MainActivity;
 import com.fooledkiwi.projectqapacapp.R;
@@ -20,14 +21,6 @@ import com.fooledkiwi.projectqapacapp.R;
  */
 public class AuthLoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AuthLoginFragment() {
         // Required empty public constructor
@@ -45,8 +38,8 @@ public class AuthLoginFragment extends Fragment {
     public static AuthLoginFragment newInstance(String param1, String param2) {
         AuthLoginFragment fragment = new AuthLoginFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        // args.putString(ARG_PARAM1, param1);
+        // args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,10 +47,6 @@ public class AuthLoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -66,8 +55,46 @@ public class AuthLoginFragment extends Fragment {
         Button loginButton = view.findViewById(R.id.btn_loginConfirm);
         loginButton.setOnClickListener(v -> {
             Intent gotoMain = new Intent(view.getContext(), MainActivity.class);
-            startActivity(gotoMain);
+            if(CheckLogin(v)) startActivity(gotoMain);
         });
         return view;
+    }
+
+    public boolean CheckLogin(View vw) {
+        EditText inputUser = vw.findViewById(R.id.editText_loginUser);
+        EditText inputPassword = vw.findViewById(R.id.editText_loginPassword);
+
+        String username = inputUser.getText().toString().trim();
+        String password = inputPassword.getText().toString();
+
+        if (username.isEmpty()) {
+            inputUser.setError(getString(R.string.error_empty_user));
+            inputUser.requestFocus();
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            inputPassword.setError(getString(R.string.error_empty_password));
+            inputPassword.requestFocus();
+            return false;
+        }
+
+        if (password.length() < 4) {
+            inputPassword.setError(getString(R.string.error_short_password));
+            inputPassword.requestFocus();
+            return false;
+        }
+
+
+        // Ejemplo de lo que deberías llamar aquí (usando tu SqlLitePlates u otro Helper):
+        // boolean loginExitoso = miDatabaseHelper.validarUsuario(username, password);
+        //
+        // if (loginExitoso) {
+        //     Toast.makeText(getContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
+        //     // Navegar a la siguiente pantalla...
+        // } else {
+        //     Toast.makeText(getContext(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+        // }
+        return  true;
     }
 }
